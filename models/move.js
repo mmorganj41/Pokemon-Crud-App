37587d498f9447b78e4e8793312eb3eef03c4ab0
mmorganj41 <mmorganj41@gmail.com>
2022-12-06 16:33:54 -0500

    move model draft

---
+++ models/move.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const moveSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		accuracy: Number,
+		type: {
+			type: String,
+			required: true,
+			enum: ["normal", "fighting","flying","poison","ground","rock","bug","ghost", "steel", "fire", "water", "grass", "electric", "psychic", "ice", "dragon", "dark"],
+		},
+		damageClass: {
+			type: String,
+			enum: ['special', 'physical']
+		},
+		experience: {
+			type: Number,
+			default: 0,
+			min: 0,
+		}
+		power: Number,
+		pp: {
+			type: Number,
+			min: 0,
+			required: true,
+		},
+		target: {
+			type: String,
+			required: true,
+		},
+		statchange
+	}
+)
+
+module.exports = mongoose.model('Move', moveSchema);
--- models/pokemon.js
+++ models/pokemon.js
@@ -14,6 +14,12 @@ const pokemonSchema = new Schema(
 			enum: ["normal", "fighting","flying","poison","ground","rock","bug","ghost", "steel", "fire", "water", "grass", "electric", "psychic", "ice", "dragon", "dark"],
 		},
 		moves: [{type: Schema.Types.ObjectId, ref: 'Move'}],
+		experience: {
+			type: Number,
+			min: 0,
+			default: 0,
+			required: true,
+		},
 		HP: {
 			type: Number,
 			min: 0,
@@ -55,6 +61,9 @@ const pokemonSchema = new Schema(
 			enum: ["hardy", "bold", "modest", "calm", "timid", "lonely", "docile", "mild", "gentle", "hasty", "adamant", "impish", "bashful", "careful", "rash", "jolly", "naughty", "lax", "quirky", "naive"]
 		},
 		trainer: {type: Schema.Types.ObjectId, ref: 'User'};
+	}, 
+	{
+		timestamps: true,
 	}
 )
 
