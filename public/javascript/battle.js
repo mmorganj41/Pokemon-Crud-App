261775908329a997872f73b9e2c6a670d916f3ac
mmorganj41 <mmorganj41@gmail.com>
2022-12-07 14:51:41 -0500

    initial battle.js framework

---
+++ public/javascript/battle.js
@@ -0,0 +1,75 @@
+// Constants
+
+const moveDamage = {
+	normal: {
+		rock: .5,
+		ghost: 0,
+		steel: .5,
+	},
+	fire: {
+		fire: .5,
+		water: .5,
+		grass: 2,
+		ice: 2,
+		bug: 2,
+		rock: .5,
+		dragon: .5,
+		steel: 2,
+	},
+	water: {
+		fire: 2,
+		water: .5,
+		grass: .5,
+		ground: 2,
+		rock: 2,
+		dragon: .5,
+	},
+	electric: {
+		water: 2,
+		electric: .5,
+		grass: .5,
+		ground: 0,
+		flying: 2,
+		dragon: .5,
+	},
+	grass: {
+		fire: .5,
+		water: 2,
+		grass: .5,
+		poison: .5,
+		ground: 2,
+		flying: .5,
+		bug: .5,
+		rock: 2,
+		dragon: .5,
+		steel: .5,
+	},
+	ice: {
+		fire: .5,
+		water: .5,
+		grass: 2,
+		ice: .5,
+		ground: 2,
+		flying: 2,
+		dragon: 2,
+		steel: .5,
+	}
+}
+
+// Global Variables
+
+// Dom elements
+
+// Callback Functions
+
+// Helper Functions
+
+// Main Functions
+
+function init() {
+
+}
+
+function render() {
+
+}
--- routes/index.js
+++ routes/index.js
@@ -16,15 +16,15 @@ router.get('/auth/google', passport.authenticate(
 router.get('/oauth2callback', passport.authenticate(
   'google',
   {
-    successRedirect : '/', // UPDATE THIS, where do you want the client to go after you login 
-    failureRedirect : '/' //  UPDATE THIS, where do you want the client to go if login fails
+    successRedirect : '/pokemon', // UPDATE THIS, where do you want the client to go after you login 
+    failureRedirect : '/pokemon' //  UPDATE THIS, where do you want the client to go if login fails
   }
 ));
 
 // OAuth logout route
 router.get('/logout', function(req, res){
   req.logout(function(){ //< - req.logout comes from passport, and what it does is destorys the cookie keeping track of the user!
-    res.redirect('/') // <---- UPDATE THIS TO WHERE YOU WANT THE USER TO GO AFTER LOGOUT
+    res.redirect('/pokemon') // <---- UPDATE THIS TO WHERE YOU WANT THE USER TO GO AFTER LOGOUT
   })
 })
 
