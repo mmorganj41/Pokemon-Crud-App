fb9a2a870f5227c396f0ee2cdc2793f388a494d4
mmorganj41 <mmorganj41@gmail.com>
2022-12-15 15:45:06 -0500

    setup trade controller frame

---
+++ controllers/trades.js
@@ -0,0 +1,7 @@
+const Trade = require('../models/trade');
+const Pokemon = require('../models/pokemon');
+const Move = require('../models/move');
+const User = require('../models/user');
+const dataFunctions = require('../config/datafunctions');
+
+module.exports = {}
